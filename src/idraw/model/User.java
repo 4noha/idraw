570581e06a34f09560c747150fd428ed3f55636a
matkat <a@p>
2016-11-18 11:12:14 +0900

    [add]Userクラスにセッションid格納用変数とfindBySessionIdメソッド（セッションid格納確認メソッド）を追加

--- src/idraw/model/User.java
+++ src/idraw/model/User.java
@@ -1,7 +1,28 @@
 package idraw.model;
 
-import idraw.orm.DbInstanceDao;
-import idraw.orm.DbStaticDao;
+import java.io.IOException;
 
-public class User {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class User{
+
+	//セッションid格納用変数
+	public static String user;
+
+	//ユーザーのセッションIDを返すメソッド
+	//仮引数：httpサーブレットのリクエスト・レスポンスのインスタンス
+	//戻り値：TRUE/FALSE
+	public static boolean findBySessionId(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		HttpSession session = req.getSession();
+		//userにセッションid値を代入
+		user = (String)req.getAttribute("SESSION-ID");
+		if(user.equals(null)){
+			return false;
+		}
+		return true;
+	}
 }
