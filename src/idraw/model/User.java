d413750675271bb0a351c36023caf49f19fe29c1
takepin <oyasaimorimori0831@yahoo.co.jp>
2016-11-18 15:13:43 +0900

    [add]ユーザーネームのバリデーターをUserに埋め込む

--- src/idraw/model/User.java
+++ src/idraw/model/User.java
@@ -7,8 +7,8 @@ import java.util.Map;
 import idraw.orm.DbInstanceDao;
 import idraw.orm.DbStaticDao;
 
-public class User extends DbInstanceDao<User>{
-	//ORMはTABLE_NAME, THIS_CLASSの入力とカラムと同名のフィールドを定義すれば後は大体コピペで使える
+public class User extends DbInstanceDao<User> {
+	// ORMはTABLE_NAME, THIS_CLASSの入力とカラムと同名のフィールドを定義すれば後は大体コピペで使える
 
 	// ORMでクラスをテーブルにマッピングするのに必要
 	private final static String TABLE_NAME = "user";
@@ -24,35 +24,40 @@ public class User extends DbInstanceDao<User>{
 	public String secret_key;
 	public String session_id;
 
-	//newしてsaveするとcreateが走る
+	// newしてsaveするとcreateが走る
 	public User() throws SQLException {
 		super(THIS_CLASS, TABLE_NAME);
 		adapterStandby();
 	}
+
 	public User(Map<String, Object> params) throws SQLException {
 		super(THIS_CLASS, TABLE_NAME);
 		adapterStandby();
-		this.username   = (String) params.get("username");
-		this.pwd   = (String) params.get("pwd");
-		this.salt   = (String) params.get("salt");
-		this.secret_key   = (String) params.get("secret_key");
-		this.session_id   = (String) params.get("session_id");
+		this.username = (String) params.get("username");
+		this.pwd = (String) params.get("pwd");
+		this.salt = (String) params.get("salt");
+		this.secret_key = (String) params.get("secret_key");
+		this.session_id = (String) params.get("session_id");
 		newFlag = true;
 	}
 
 	// 検索関数を定義
-	public static User findBy(String column_name, Object value) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchFieldException, SecurityException{
+	public static User findBy(String column_name, Object value) throws SQLException, InstantiationException,
+			IllegalAccessException, ClassNotFoundException, NoSuchFieldException, SecurityException {
 		adapterStandby();
 		return dbStaticAdapter.findBy(column_name, value);
 	}
-	public static ArrayList<User> findPartial(String column_name, String value) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchFieldException, SecurityException{
+
+	public static ArrayList<User> findPartial(String column_name, String value)
+			throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException,
+			NoSuchFieldException, SecurityException {
 		adapterStandby();
 		return dbStaticAdapter.findPartial(column_name, value);
 	}
 
 	// クラスの設定が初期化されていないときに呼ばれる
-	private static void adapterStandby() throws SQLException{
-		if (dbStaticAdapter == null || dbAdapter == null){
+	private static void adapterStandby() throws SQLException {
+		if (dbStaticAdapter == null || dbAdapter == null) {
 			// クラスメソッドを使うための設定
 			dbStaticAdapter = new DbStaticDao<User>(THIS_CLASS, TABLE_NAME);
 			// インスタンスメソッドを使うための設定
@@ -63,7 +68,17 @@ public class User extends DbInstanceDao<User>{
 	}
 
 	// クラスのモデルのカラム名一覧を取得
-	public static ArrayList<String> getColumnNames(){
+	public static ArrayList<String> getColumnNames() {
 		return columnNames;
+	}
+
+	// ユーザーネームのバリデーター(英数字20字まで)
+	public boolean AcceptableName(String userName) {
+		int a = userName.length();
+		if (a <= 20) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
