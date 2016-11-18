99c560d1ea3a302c8b543ba0f30449121f896d33
hentaro <takethetime1989@gmail.com>
2016-11-18 10:58:02 +0900

    [add] １．JSONライブラリの追加  ・jackson-core-2.8.1.jar、  ・jackson-databind-2.8.5.jar、  ・jackson-annotations-2.8.0.jar
    ２．package idraw　の　WebsocketEndpointクラス　の　onMessageメソッドにJSON（文字列ベースのデータ
    フォーマット）形式の文字列をHashMapインスタンスに＜Command名:値＞に変換する処理を追加

--- src/idraw/WebsocketEndpoint.java
+++ src/idraw/WebsocketEndpoint.java
@@ -2,13 +2,17 @@ package idraw;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  *
@@ -16,13 +20,16 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/endpoint")
 public class WebsocketEndpoint {
     static Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
-    
+
     @OnMessage
     public void onMessage(String message) throws IOException {
     	////
     	//    送信以外の処理はこのへん(Synchronizedの外)で書きます
     	////
-    	
+    	//JSON（文字列ベースのデータフォーマット）形式の文字列をHashMapインスタンスに＜Command名:値＞にパースする 2016/11/18高田追加
+    	HashMap<String, Object> parsedJson = new ObjectMapper().readValue(message, HashMap.class);
+
+
 		// 送信中はセマフォで他のプロセスにアクセスされないようにする
     	synchronized(sessions) {
     		System.out.println("セッション数(" + sessions.size() + ")" + message);
@@ -32,7 +39,7 @@ public class WebsocketEndpoint {
 	    	}
         }
     }
-    
+
     @OnOpen
     public void open(Session sess){
         sessions.add(sess);
