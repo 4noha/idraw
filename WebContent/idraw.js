294adef3df199870da83eca5a7bb83eaa3778400
nokkii <nokkii.03@gmail.com>
2016-11-18 13:45:35 +0900

    [clean] 不要なコメントを削除

--- WebContent/idraw.js
+++ WebContent/idraw.js
@@ -42,11 +42,6 @@ $(function() {
     		break;
     	}
     };
-    
-    /*
-    socket.on('clear user', function () {
-        context.clearRect(0, 0, $('canvas').width(), $('canvas').height());
-    });*/
  
     $('canvas').mousedown(function(e) {
         drawFlag = true;
