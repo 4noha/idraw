4ffc147ab9702faf01891860f1b0e12bf395cc9e
hentaro <takethetime1989@gmail.com>
2016-11-21 11:11:37 +0900

    [add]idraw.WebsocketEndpoint.onMessageメソッドに処理を追加 １．cmd が login 且つ JSON内に pwd, session_id がある場合のif文を追加（処理内容未記載） ２．cmd が save,login の際の共通コードをメソッド化

--- src/idraw/WebsocketEndpoint.java
+++ src/idraw/WebsocketEndpoint.java
@@ -18,6 +18,7 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import idraw.manager.EncryptManager;
@@ -36,9 +37,6 @@ public class WebsocketEndpoint {
 	public void onMessage(String message) throws IOException, InstantiationException, IllegalAccessException,
 			ClassNotFoundException, NoSuchFieldException, SecurityException, SQLException, IllegalArgumentException,
 			NoSuchMethodException, InvocationTargetException, NoSuchAlgorithmException {
-		////
-		// 送信以外の処理はこのへん(Synchronizedの外)で書きます
-		////
 
 		// JSON（文字列ベースのデータフォーマット）形式の文字列をHashMapインスタンスに＜Command名:値＞にパースする
 		HashMap<String, Object> parsedJson = new ObjectMapper().readValue(message, HashMap.class);
@@ -46,12 +44,14 @@ public class WebsocketEndpoint {
 
 		switch (cmd) {
 		case "pen": // cmd = pen の場合、DBへの保存やpen自体の新規作成はないため何もしない
+		case "image": // cmd = image の場合、Websocket上では何もせずクライアントへ送信
 			break;
 
 		case "save": // cmd = save の場合、状況により新規作成 or 上書きをする
-			Page page = Page.findBy("page_num", parsedJson.get("page"));
+			Page page = Page.findBy("page_num", parsedJson.get("page_num"));
 			if (page == null) { // pageが何も無ければ新規作成する
 				page = new Page(toMap(m -> {
+					m.put("page_num", parsedJson.get("page_num"));
 					m.put("page", parsedJson.get("page"));
 					m.put("joined_image", parsedJson.get("image"));
 				}));
@@ -59,41 +59,50 @@ public class WebsocketEndpoint {
 				page.joined_image = (String) parsedJson.get("image");
 			}
 			page.save();
+			final int pageNum = page.page_num;
+			message = mapToJsonString(m -> {
+				m.put("cmd", "save");
+				m.put("page", pageNum);
+			});
 			break;
 
 		case "login": // cmd = login の場合、ログイン判定を行う
-			String userName = (String) parsedJson.get("id");
-			boolean cmdNew = (boolean) parsedJson.get("new");
-			User user = null;
-			if (cmdNew == true) { //ユーザの新規作成をするための処理
-				user = new User();
-				user.username = userName;
-				user.save();
-			} else { //ユーザ検索の結果ユーザが存在した場合にPublicKeyを作成し”key”として返却するための処理
-				user = User.findBy("username", parsedJson.get("id"));
-				if (user == null) {
-					message = "{ \"cmd\":\"error\", \"key\":\"IDが見つかりません\" }";
-					break;
-				}
-			}
-			String publicKey = EncryptManager.generateKeyPair(user);
-			Map<Object, Object> json = toMap(m -> {
-				m.put("cmd", "pubkey");
-				m.put("key", publicKey);
-			});
-			ObjectMapper om = new ObjectMapper();
-			message = om.writeValueAsString(json);
-			break;
 
-		case "image": // cmd = image の場合、Websocket上では何もせずクライアントへ送信
-			break;
+			// 来たJSONから情報を読み取る
+			String userName = (String) parsedJson.get("id");
+			String pwd = (String) parsedJson.get("pwd");
+			String session_id = (String) parsedJson.get("session_id");
+			boolean cmdNew = (boolean) parsedJson.get("new");
+
+			if (userName != null && pwd != null && session_id != null) { // 来たJSONがcmd=login且つ情報内にpwdとsession_idがあれば
+
+			} else { // 来たJSON内にidはあるがpwd,session_id情報が無ければpwd,publicKeyを返す
+				User user = null;
+				if (cmdNew == true) { // ユーザの新規作成をするための処理
+					user = new User();
+					user.username = userName;
+					user.save();
+				} else { // ユーザ検索の結果ユーザが存在した場合にPublicKeyを作成し”key”として返却するための処理
+					user = User.findBy("username", parsedJson.get("id"));
+					if (user == null) {
+						message = "{ \"cmd\":\"error\", \"key\":\"IDが見つかりません\" }";
+						break;
+					}
+				}
+				String publicKey = EncryptManager.generateKeyPair(user);
+				message = mapToJsonString(m -> {
+					m.put("cmd", "pubkey");
+					m.put("key", publicKey);
+				});
+				break;
+			}
 
 		case "session": // cmd = session の場合、ユーザを検索し、ユーザが見つかればページを与える
 			User sessionUser = User.findBy("secret_key", parsedJson.get("id"));
 			if (sessionUser == null) {
 				message = "{ \"cmd\":\"error\", \"key\":\"ユーザが見つかりません\" }";
-			}else{
-				//ページを返す処理を記載予定
+			} else {
+				// ページを返す処理を記載予定
 			}
 			break;
 
@@ -136,5 +145,13 @@ public class WebsocketEndpoint {
 		Map<K, V> map = new LinkedHashMap<>();
 		initializer.accept(map);
 		return map;
+	}
+
+	public static <K, V> String mapToJsonString(Consumer<Map<K, V>> initializer) throws JsonProcessingException {
+		Map<K, V> map = new LinkedHashMap<>();
+		initializer.accept(map);
+
+		ObjectMapper om = new ObjectMapper();
+		return om.writeValueAsString(map);
 	}
 }
