75453da9ef553e3a140dc4140ead4623e17d830b
unknown <>
2016-11-27 22:48:36 +0900

    [add] DbUtilにテストを追加

--- src/test/idraw/orm/DbUtilTest.java
+++ src/test/idraw/orm/DbUtilTest.java
@@ -1,39 +1,43 @@
 package test.idraw.orm;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import idraw.orm.DbUtil;
 
 public class DbUtilTest {
-
-	@Before
-	public void setUp() throws Exception {
+	@Test
+	public void 正常にTestのDBに接続切断出来るかの確認() throws ClassNotFoundException, SQLException {
 		DbUtil.connect(toMap(m -> {
 			m.put("env", "test");
-			m.put("host", "127.0.0.1:3306");
 			m.put("db_name", "idraw");
 		}));
+		DbUtil.close();
+		// 正常終了すればOK
+		assertEquals(true, true);
 	}
+
+	@Test
+	public void 正常にProductionのDBに接続切断出来るかの確認() throws ClassNotFoundException, SQLException {
+		DbUtil.connect(toMap(m -> {
+			m.put("env", "production");
+			m.put("db_name", "idraw");
+		}));
+		DbUtil.close();
+		// 正常終了すればOK
+		assertEquals(true, true);
+	}
+	
 	// 簡単にMapを作る用メソッド
 	public static <K, V> Map<K, V> toMap(Consumer<Map<K, V>> initializer) {
 		Map<K, V> map = new LinkedHashMap<>();
 		initializer.accept(map);
 		return map;
 	}
-
-	@Test
-	public void 正常にDBを切断出来るかの確認() {
-		try {
-			DbUtil.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
 }
