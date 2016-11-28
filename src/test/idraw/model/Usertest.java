46f4c594a88460dd6ad14d69c3c838a04b0a6583
takepin <oyasaimorimori0831@yahoo.co.jp>
2016-11-28 09:07:58 +0900

    [add]Usertestを追加

---
+++ src/test/idraw/model/Usertest.java
@@ -0,0 +1,61 @@
+package test.idraw.model;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import idraw.model.User;
+import idraw.orm.DbUtil;
+
+public class Usertest{
+
+	@Before
+	public void setUp() throws ClassNotFoundException, SQLException {
+		DbUtil.connect(toMap(m -> {
+			m.put("env", "test");
+			m.put("password", "s551107t");
+			m.put("user", "root");
+			m.put("host", "127.0.0.1:3306");
+			m.put("db_name", "idraw");
+		}));
+	}
+	// 簡単にMapを作る用メソッド
+	public static <K, V> Map<K, V> toMap(Consumer<Map<K, V>> initializer) {
+		Map<K, V> map = new LinkedHashMap<>();
+		initializer.accept(map);
+		return map;
+	}
+	@Test
+	public void AcceptableNameでユーザー名が半角英数字20文字以内あること() {
+		// fail("まだ実装されていません");
+		try {
+			User user = new User();// インスタンスuserを作成Userコンストラクタ呼び出し
+			String userName = "a";// userNameをaで初期化
+			boolean expected = true;
+			boolean actual;
+			actual = user.AcceptableName(userName);
+			assertThat(actual, is(expected));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			User user = new User();// インスタンスuserを作成Userコンストラクタ呼び出し
+			String userName = "あ";// userNameをあで初期化
+			boolean expected = false;
+			boolean actual;
+			actual = user.AcceptableName(userName);
+			assertThat(actual, is(expected));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
